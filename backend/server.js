f8666499b663eb1eaa525cff3d922c95e59e0de2
Parrurox <shaikhparwez963@gmail.com>
2024-03-16 12:49:29 +0530

    asyncHandler added

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,14 @@ import express from "express";
 // Load env variables (this is due to older LTS version of node, newer version don't need this line of code)
 import dotenv from "dotenv";
 dotenv.config();
+import userRoutes from "./routes/userRoutes.js";
 
 const port = process.env.PORT || 3000;
 // Create an express application
 const app = express();
+
+app.use("/api/users", userRoutes); // Mount the userRoutes on /api/users
+
 app.get("/", (req, res) => {
   res.send("api is running");
 });

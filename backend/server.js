8d60b5d4165d7336465a543c4f29a7eb5825ae7b
Parrurox <shaikhparwez963@gmail.com>
2024-03-19 18:08:19 +0530

    app finished

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
+import path from "path"; // Node.js module for resolving file paths (after building frontend)
 
 // Load env variables (this is due to older LTS version of node, newer version don't need this line of code)
 import dotenv from "dotenv";
@@ -18,10 +19,19 @@ app.use(express.json()); // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(cookieParser());
 
-//test
-app.get("/", (req, res) => {
-  res.send("api is running");
-});
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 //------------------Routes------------------
 app.use("/api/users", userRoutes); // Mount the userRoutes on /api/users
